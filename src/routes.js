bbe5cb438ef71779a1981aafdb837c0b65f01446
luanslf <fernaul19>
2020-05-07 11:42:19 -0300

    Guardas de rota + Vue Router Multiguard

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,41 @@
+import multiguard from 'vue-router-multiguard';
+
 import Home from './pages/Home.vue';
 import Login from './pages/Login.vue';
 import Dashboard from './pages/Dashboard.vue';
 
 import DashboardHome from './components/dashboard/DashboardHome.vue';
 
+const authenticated = (to, from, next) => {
+  if (localStorage.getItem('authenticated')) {
+    next();
+  } else {
+    next({
+      path: '/',
+    });
+  }
+};
+
+const unauthenticated = (to, from, next) => {
+  if (!localStorage.getItem('authenticated')) {
+    next();
+  } else {
+    next({
+      path: '/',
+    });
+  }
+};
+
 export default [
   {
     path: '/login',
     component: Login,
+    beforeEnter: multiguard([unauthenticated]),
   },
   {
     path: '/dashboard',
     component: Dashboard,
-    beforeEnter(to, from, next) {
-      if (localStorage.getItem('authenticated')) {
-        next();
-      } else {
-        next({ path: '/' });
-      }
-    },
+    beforeEnter: multiguard([authenticated]),
     children: [
       {
         path: '/',
